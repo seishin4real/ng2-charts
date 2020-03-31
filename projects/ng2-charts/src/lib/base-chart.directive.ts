ecb8a059a65e89463002aed4197b918a050bc0ef
Łukasz Sobków <seishin@o2.pl>
2020-03-31 11:43:02 +0200

    updated lodash-es impr. dependencies

--- projects/ng2-charts/src/lib/base-chart.directive.ts
+++ projects/ng2-charts/src/lib/base-chart.directive.ts
@@ -15,7 +15,7 @@ import { getColors } from './get-colors';
 import { Color } from './color';
 import { ThemeService } from './theme.service';
 import { Subscription } from 'rxjs';
-import { cloneDeep } from 'lodash-es';
+import cloneDeep from 'lodash-es/cloneDeep';
 
 export type SingleDataSet = (number[] | chartJs.ChartPoint[]);
 export type MultiDataSet = (number[] | chartJs.ChartPoint[])[];
